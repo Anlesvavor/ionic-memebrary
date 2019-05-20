2ec68cad7a2154e3ceed648e74668509b54f6644
Jesus Jose Saenz Fierro <jaredmaster32@gmail.com>
2019-05-20 08:27:32 -0600

    Mi primer commit.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import {UploadComponent} from '../upload/upload.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, UploadComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Image} from '../model/Image';
+import {PseudoImageService} from '../api/pseudo-image.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  images: any = [];
+
+  constructor(private imageService: PseudoImageService) {
+  }
+
+  ionViewWillEnter() {
+    //console.log(this.imageService.getImage());
+    this.imageService.getImage().subscribe(images => {
+      this.images = images;
+    });
+
+    this.images = this.imageService.getImage();
+    console.log(this.images);
+  }
 
 }
